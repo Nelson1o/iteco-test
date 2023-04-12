e92e62557fe4fbb4d66ed3886f935ffc90b57fb2
Anton <anton.krivozubov@mail.ru>
2023-04-12 11:47:31 +0300

    feat: localstorage

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -5,23 +5,47 @@ import styles from "./Form.module.scss";
 type TForm = {
   id: number;
   type: string;
+  text: string | number;
   removeForm: (id: number) => void;
+  changeTypeForm: (e: React.ChangeEvent<HTMLSelectElement>, id: number) => void;
+  onChangeValue: (e: React.ChangeEvent<HTMLInputElement>, id: number) => void;
 };
 
-const Form: React.FC<TForm> = ({ id, type, removeForm }) => {
+const Form: React.FC<TForm> = ({ id, type, text, removeForm, changeTypeForm, onChangeValue }) => {
   return (
-    <div className={styles.formItem}>
-      <input type={type} className={styles.input} />
-      <input type={type} className={styles.input} />
+    <div className={styles.formItems}>
+      <div className={styles.formItem}>
+        <label htmlFor={id.toString()}>Full Name</label>
+        <input
+          type={type}
+          className={styles.input}
+          placeholder={type}
+          id={id.toString()}
+          value={text}
+          onChange={(e) => onChangeValue(e, id)}
+        />
+      </div>
 
-      <button className={styles.closeButton} onClick={() => removeForm(id)}>
-        <svg height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg">
-          <path d="M38 12.83l-2.83-2.83-11.17 11.17-11.17-11.17-2.83 2.83 11.17 11.17-11.17 11.17 2.83 2.83 11.17-11.17 11.17 11.17 2.83-2.83-11.17-11.17z" />
-          <path d="M0 0h48v48h-48z" fill="none" />
-        </svg>
-      </button>
+      <div className={styles.changeForm}>
+        <select
+          defaultValue={type}
+          onChange={(e) => changeTypeForm(e, id)}
+          className={styles.select}
+          name="formType"
+        >
+          <option value="text">text</option>
+          <option value="checkbox">checkbox</option>
+          <option value="email">email</option>
+          <option value="password">password</option>
+        </select>
 
-      {/* <button className={styles.changeButton}>Change</button> */}
+        <button className={styles.closeButton} onClick={() => removeForm(id)}>
+          <svg height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg">
+            <path d="M38 12.83l-2.83-2.83-11.17 11.17-11.17-11.17-2.83 2.83 11.17 11.17-11.17 11.17 2.83 2.83 11.17-11.17 11.17 11.17 2.83-2.83-11.17-11.17z" />
+            <path d="M0 0h48v48h-48z" fill="none" />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/FormBuilder/FormBuilder.tsx
+++ src/components/FormBuilder/FormBuilder.tsx
@@ -1,38 +1,92 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Form from "../Form/Form";
+import Loader from "../Loader";
 
 import styles from "./FormBuilder.module.scss";
 
+interface IFormState {
+  id: number;
+  type: string;
+  text: number | string;
+}
+
 const FormBuilder: React.FC = () => {
-  const [forms, setForms] = useState([
-    {
-      id: Math.random(),
-      type: "text",
-      // className
-    },
-  ]);
+  const [forms, setForms] = useState<IFormState[]>([]);
 
   const addForm = () => {
-    setForms((prev) => [...prev, { id: Math.random(), type: "text" }]);
+    setForms((prev) => [...prev, { id: Math.random(), type: "text", text: "" }]);
   };
 
   const removeForm = (id: number) => {
     setForms((prev) => prev.filter((item) => item.id !== id));
   };
 
+  const onChangeValue = (e: React.ChangeEvent<HTMLInputElement>, id: number) => {
+    setForms((prev) =>
+      prev.map((item) => {
+        if (item.id === id) {
+          item.text = e.target.value;
+        }
+        return item;
+      })
+    );
+  };
+
+  const changeTypeForm = (e: React.ChangeEvent<HTMLSelectElement>, id: number) => {
+    setForms((prev) =>
+      prev.map((item) => {
+        if (item.id === id) {
+          item.type = e.target.value;
+        }
+        return item;
+      })
+    );
+  };
+
+  useEffect(() => {
+    const items = localStorage.getItem("formsItems");
+    if (items) {
+      const parse: IFormState[] = JSON.parse(items!);
+      setForms((prev) => [...prev, ...parse]);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (forms.length !== 0) {
+      localStorage.setItem("formsItems", JSON.stringify(forms));
+    }
+  }, [forms]);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(forms);
+  };
+
   return (
-    <div>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <div className={styles.buttonItems}>
-        <button className={styles.addButton} onClick={addForm}>
+        <button className={styles.addButton} /* type="button" */ onClick={addForm}>
           Add
         </button>
-        <button className={styles.addButton}>Submit</button>
+        <button className={styles.addButton} type="submit">
+          Submit
+        </button>
       </div>
 
-      {forms.map((form) => (
-        <Form key={form.id} removeForm={removeForm} {...form} />
-      ))}
-    </div>
+      {forms.length !== 0 ? (
+        forms.map((form) => (
+          <Form
+            key={form.id}
+            removeForm={removeForm}
+            changeTypeForm={changeTypeForm}
+            onChangeValue={onChangeValue}
+            {...form}
+          />
+        ))
+      ) : (
+        <Loader />
+      )}
+    </form>
   );
 };
 
